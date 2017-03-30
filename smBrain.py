836b15686eddcd0d043eacbb5d388296688b1e38
slzh645 <slzh645@gmail.com>
2017-03-30 16:56:12 +0800

    Add files via upload

--- smBrain.py
+++ smBrain.py
@@ -2,56 +2,42 @@ import math
 import libdw.util as util
 import libdw.sm as sm
 import libdw.gfx as gfx
-import math as m
 from soar.io import io
 
 class MySMClass(sm.SM):
-    startState = 0
+    startState=0
     def getNextValues(self, state, inp):
-        p = 0
         if state == 0:
-            print state
-            print p
-            if p == 0:
-                if inp.sonars[2]< 0.3:
-                    nextstate = 0
-                    output = (io.Action(fvel = -0.5, rvel = 0))
-                elif inp.sonars[2]>=0.5:
-                    nextstate = 0
-                    output = (io.Action(fvel = 0.5, rvel = 0))
-                elif inp.sonars[2]>=0.3 and inp.sonars[2]<0.5:
-                    nextstate = 1
-                    output = (io.Action(fvel = 0, rvel = 0.2))
-            elif p == 1: 
-                if inp.sonars[2]>=0.3 and inp.sonars[2]<0.5:
-                    nextstate = 1
-                    output = (io.Action(fvel = 0, rvel = 0.2))
-                if inp.sonars[4]>0.5 and inp.sonars[3]>0.75:
-                    nextstate = 2
-                    output = (io.Action(fvel = 0, rvel = -0.2))
-        elif state == 1: 
-            v = inp.sonars[3]
-            c = m.sqrt(2)*inp.sonars[4]
-            print v
-            print c
-            if v >= c-0.005 and v < c+0.005:
-                p = 1
-                nextstate = 0
-                output = (io.Action(fvel = 0.5, rvel = 0))
-            else: 
-                nextstate = 1
-                output = (io.Action(fvel = 0, rvel = 0.2))
-        elif state == 2: 
-            print state
-            if inp.sonars[2]>1 and inp.sonars[3]<0.75:
-                nextstate = 1
-                output = (io.Action(fvel = 0.5, rvel = 0))
-            else: 
-                nextstate = 2
-                output = (io.Action(fvel = 0, rvel = -0.2))
+            if inp.sonars[2] > 0.5:
+                nextState = 0
+                outp = io.Action(fvel = 0.1, rvel = 0.00)
+            elif inp.sonars[2] <= 0.5:
+                nextState = 1
+                outp = io.Action(fvel = 0.05, rvel = 0.0)
                 
-        return(nextstate, output)
-            
+        elif state == 1:
+            if inp.sonars[2] >0.5 and inp.sonars[3] >0.5 and inp.sonars[4] >0.3:
+                nextState = 2
+                outp = io.Action(fvel = 0.1, rvel = -0.1)
+            elif inp.sonars[2] >0.5 and inp.sonars[3] > 0.3:
+                nextState = 1
+                outp = io.Action(fvel = 0.1, rvel = 0.0)
+            elif inp.sonars[2] > 0.3:
+                nextState = 1
+                outp = io.Action(fvel = 0.00, rvel = 0.1)
+
+                
+        elif state == 2:
+            if inp.sonars[2] >0.5 and inp.sonars[3] > 0.3:
+                nextState = 2 
+                outp = io.Action(fvel = 0.05, rvel = -0.1)
+            else:
+                nextState = 1
+                outp = io.Action(fvel = 0.1, rvel = 0.0)
+
+        print inp.sonars[2]
+        print inp.odometry.theta
+        return (nextState, outp)
 
 mySM = MySMClass()
 mySM.name = 'brainSM'
@@ -70,7 +56,7 @@ def plotSonar(sonarNum):
 # this function is called when the brain is (re)loaded
 def setup():
     robot.gfx = gfx.RobotGraphics(drawSlimeTrail=True, # slime trails
-                                  sonarMonitor=False) # sonar monitor widget
+                                  sonarMonitor=True) # sonar monitor widget
     
     # set robot's behavior
     robot.behavior = mySM
